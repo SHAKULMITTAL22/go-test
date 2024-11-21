f44cdec59fe163d156a14fdb4a87ef0aaf232a18
SHAKULMITTAL22 <80421896+SHAKULMITTAL22@users.noreply.github.com>
2024-11-21 16:32:08 +0530

    Update addition.go

--- addition.go
+++ addition.go
@@ -1,12 +1,16 @@
 package main
 
 func Mervin1(a int) string {
+	var result string // Declare a variable to hold the return value
 
 	if a == 1 {
-		return "x"
+		result = "x"
 	} else if a == 2 {
-		return "y"
+		result = "y"
+	} else {
+		result = "z"
 	}
 
-	return "z"
+	return result // Return the variable
 }
+
